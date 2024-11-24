967c4d45de7d077f30ea640fa12347d38d5a980e
Felix Hilgers <felix.hilgers@fau.de>
2024-11-24 14:42:47 +0100

    feat(b04): check file preconditions
    Signed-off-by: Felix Hilgers <felix.hilgers@fau.de>

--- src/adap-b04/files/File.ts
+++ src/adap-b04/files/File.ts
@@ -1,5 +1,6 @@
 import { Node } from "./Node";
 import { Directory } from "./Directory";
+import { InvalidStateException } from "../common/InvalidStateException";
 
 enum FileState {
     OPEN,
@@ -13,18 +14,53 @@ export class File extends Node {
 
     constructor(baseName: string, parent: Directory) {
         super(baseName, parent);
+        this.assertClassInvariants();
     }
 
     public open(): void {
-        // do something
+        this.assertClassInvariants();
+        // I have explicitly opted to check the negatives to provide better error messages
+        this.assertIsNotInState([FileState.DELETED, FileState.OPEN]);
+        this.assertIsInState(FileState.CLOSED);
+
+        this.doSetFileState(FileState.OPEN);
+        
+        this.assertIsInState(FileState.OPEN);
     }
 
     public close(): void {
-        // do something
+        this.assertClassInvariants();
+        // I have explicitly opted to check the negatives to provide better error messages
+        this.assertIsNotInState([FileState.DELETED, FileState.CLOSED]);
+        this.assertIsInState(FileState.OPEN);
+
+        this.doSetFileState(FileState.CLOSED);
+        
+        this.assertIsInState(FileState.CLOSED);
     }
 
     protected doGetFileState(): FileState {
         return this.state;
     }
+    
+    protected doSetFileState(state: FileState): void {
+        this.state = state;
+    }
 
+    protected assertIsNotInState(states: FileState[]) {
+        for (const state of states) {
+            InvalidStateException.assertCondition(this.doGetFileState() !== state, `file is ${this.doGetFileState()}`);
+        }
+    }
+    
+    protected assertIsInState(state: FileState) {
+        InvalidStateException.assertCondition(this.doGetFileState() === state, `file should be ${state} but is ${this.doGetFileState()}`);
+    }
+    
+    protected assertClassInvariants() {
+        InvalidStateException.assertCondition(
+            this.doGetFileState() == FileState.CLOSED || this.doGetFileState() == FileState.OPEN || this.doGetFileState() == FileState.DELETED,
+            `file is in unknown state ${this.doGetFileState()}`
+        );
+    }
 }
